ac383980e4da15926b3778291f4ce870ab513f38
Ng Kitt Horng, Zion <zion@intzone.com>
2014-09-13 19:23:44 +0800

    Output only if navigation menu is set

--- znwp_bootstrap_theme/navigation.php
+++ znwp_bootstrap_theme/navigation.php
@@ -31,11 +31,14 @@ global $znwp_theme;
 
             <nav class="collapse navbar-collapse" role="navigation">
               <?php
-              wp_nav_menu(array(
-                  'theme_location' => 'primary',
-                  'container'      => false,
-                  'menu_class'     => 'nav navbar-nav',
-              ));
+              // Output only if navigation menu is set
+              if (has_nav_menu('primary')) {
+                  wp_nav_menu(array(
+                      'theme_location' => 'primary',
+                      'container'      => false,
+                      'menu_class'     => 'nav navbar-nav',
+                  ));
+              }
               ?>
             </nav>
           </div>
