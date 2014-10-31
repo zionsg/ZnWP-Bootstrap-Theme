e0fddec831f09b78c30f9942a36839d622ce244e
Ng Kitt Horng, Zion <zion@intzone.com>
2014-10-31 14:17:33 +0800

    Use div instead of h1 for site title

--- znwp_bootstrap_theme/header-top.php
+++ znwp_bootstrap_theme/header-top.php
@@ -13,7 +13,7 @@ global $znwp_theme;
               <div class="<?php echo $znwp_theme->get_full_width_class(); ?>">
                 <?php if ($znwp_theme->display_header_text()): ?>
                   <div id="header-text">
-                    <h1 class="site-title"><?php bloginfo('name'); ?></h1>
+                    <div class="site-title"><?php bloginfo('name'); ?></div>
                     <div class="tagline"><?php bloginfo('description'); ?></div>
                   </div>
                 <?php endif; ?>
