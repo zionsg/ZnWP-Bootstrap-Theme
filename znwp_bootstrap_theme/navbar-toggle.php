64ffe971f8ac247674a7c0ecdd6f5962bd9f9215
Ng Kitt Horng, Zion <zion@intzone.com>
2015-02-04 00:58:54 +0800

    Create navbar-toggle.php

---
+++ znwp_bootstrap_theme/navbar-toggle.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Navbar toggle button template
+ *
+ * @package ZnWP Bootstrap Theme
+ */
+?>
+
+              <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="nav">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
