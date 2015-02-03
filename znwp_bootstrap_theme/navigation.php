b2039bf6b46f1e72807a21b3f8171ea843c69d10
Ng Kitt Horng, Zion <zion@intzone.com>
2015-02-04 00:59:30 +0800

    Use template parts for brand, toggle, menu

--- znwp_bootstrap_theme/navigation.php
+++ znwp_bootstrap_theme/navigation.php
@@ -11,37 +11,14 @@ global $znwp_theme;
         <div class="navbar navbar-default">
           <div class="container">
             <div class="navbar-header">
-              <a class="navbar-brand" href="<?php echo esc_url(home_url('/')); ?>">
-                <?php
-                $brand = $znwp_theme->theme_mod('navbar_brand');
-                if ('home-icon' == $brand) {
-                    echo '<span class="glyphicon glyphicon-home"></span>';
-                } elseif ('site-title' == $brand) {
-                    bloginfo('name');
-                } elseif ('none' == $brand) {
-                    // Show nothing
-                }
-                ?>
-              </a>
-              <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="nav">
-                <span class="sr-only">Toggle navigation</span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-              </button>
+              <?php
+              get_template_part('navbar-brand');
+              get_template_part('navbar-toggle');
+              ?>
             </div>
 
             <nav class="collapse navbar-collapse" role="navigation">
-              <?php
-              // Output only if navigation menu is set
-              if (has_nav_menu('primary')) {
-                  wp_nav_menu(array(
-                      'theme_location' => 'primary',
-                      'container'      => false,
-                      'menu_class'     => 'nav navbar-nav',
-                  ));
-              }
-              ?>
+              <?php get_template_part('navbar-menu'); ?>
             </nav>
           </div>
         </div>
