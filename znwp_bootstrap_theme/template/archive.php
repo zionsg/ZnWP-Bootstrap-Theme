4cf88107f7784c7df0caa0bfa91d6a3d204116aa
Ng Kitt Horng, Zion <zion@intzone.com>
2015-07-17 22:54:18 +0800

    Cater for yearly archives

--- znwp_bootstrap_theme/template/archive.php
+++ znwp_bootstrap_theme/template/archive.php
@@ -4,6 +4,16 @@
  *
  * @package ZnWP Bootstrap Theme
  */
+
+$title = '';
+if (is_year()) {
+    $title = get_query_var('year');
+} elseif (is_month()) {
+    $title = single_month_title(' ', false);
+} elseif (is_day()) {
+    $date = get_query_var('day') . single_month_title(' ', false);
+    $title = date('d F Y', strtotime($date));
+}
 ?>
 
-              <h1 class="page-title">Archive:<?php single_month_title(' '); ?></h1>
+              <h1 class="page-title">Archive: <?php echo trim($title); ?></h1>
