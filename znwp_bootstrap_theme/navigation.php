d776f6f3a7d4497a537e246da7a53bc20a4bc0b6
Ng Kitt Horng, Zion <zion@intzone.com>
2016-01-30 22:51:45 +0800

    Remove .container div

--- znwp_bootstrap_theme/navigation.php
+++ znwp_bootstrap_theme/navigation.php
@@ -9,16 +9,14 @@ $znwp_theme = ZnWP_Bootstrap_Theme::getInstance();
 ?>
 
         <div class="navbar navbar-default">
-          <div class="container">
-            <div class="navbar-header">
-              <?php
-              get_template_part('navbar-brand');
-              get_template_part('navbar-toggle');
-              ?>
-            </div>
-
-            <nav class="collapse navbar-collapse" role="navigation">
-              <?php get_template_part('navbar-menu'); ?>
-            </nav>
+          <div class="navbar-header">
+            <?php
+            get_template_part('navbar-brand');
+            get_template_part('navbar-toggle');
+            ?>
           </div>
+
+          <nav class="collapse navbar-collapse" role="navigation">
+            <?php get_template_part('navbar-menu'); ?>
+          </nav>
         </div>
